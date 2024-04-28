e84abf8dccd411edb6feee548b8910452f7bb2c6
atanasov18g <atanasovgeorgi9819@gmail.com>
2024-04-28 14:06:55 +0300

    Vercel errors fix

--- components/MyTypography/elements.jsx
+++ components/MyTypography/elements.jsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 import styled from "styled-components";
 
-export const StyledSectionHeader = styled(forwardRef((props, ref) => <h1 {...props} ref={ref} />))`
+const StyledSectionHeader = styled(forwardRef((props, ref) => <h1 {...props} ref={ref} />))`
 font-family: Poppins;
 font-size: 2.5rem;
 line-height: 4.375rem;
@@ -9,10 +9,18 @@ font-weight: 600;
 text-align: center;
 `;
 
-export const StyledSectionBelowHeader = styled(forwardRef((props, ref) => <h5 {...props} ref={ref} />))`
+const StyledSectionBelowHeader = styled(forwardRef((props, ref) => <h5 {...props} ref={ref} />))`
 font-family: Poppins;
 font-size: 1.25rem;
 line-height: 1.875rem;
 font-weight: 300;
 text-align: center;
 `;
+
+StyledSectionHeader.displayName = 'StyledSectionHeader';
+StyledSectionBelowHeader.displayName = 'StyledSectionBelowHeader';
+
+export default {
+    StyledSectionHeader,
+    StyledSectionBelowHeader
+}
