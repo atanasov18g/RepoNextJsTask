bc163438328c3ee30b67a6ce612b2d8257a9652f
atanasov18g <atanasovgeorgi9819@gmail.com>
2024-04-28 14:24:14 +0300

    another error attempt

--- components/MyTypography/elements.jsx
+++ components/MyTypography/elements.jsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 import styled from "styled-components";
 
-const StyledSectionHeader = styled(forwardRef((props, ref) => <h1 {...props} ref={ref} />))`
+export const StyledSectionHeader = styled(forwardRef((props, ref) => <h1 {...props} ref={ref} />))`
 font-family: Poppins;
 font-size: 2.5rem;
 line-height: 4.375rem;
@@ -9,7 +9,7 @@ font-weight: 600;
 text-align: center;
 `;
 
-const StyledSectionBelowHeader = styled(forwardRef((props, ref) => <h5 {...props} ref={ref} />))`
+export const StyledSectionBelowHeader = styled(forwardRef((props, ref) => <h5 {...props} ref={ref} />))`
 font-family: Poppins;
 font-size: 1.25rem;
 line-height: 1.875rem;
@@ -20,7 +20,3 @@ text-align: center;
 StyledSectionHeader.displayName = 'StyledSectionHeader';
 StyledSectionBelowHeader.displayName = 'StyledSectionBelowHeader';
 
-export default {
-    StyledSectionHeader,
-    StyledSectionBelowHeader
-};
